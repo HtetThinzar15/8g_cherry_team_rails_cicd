f4dbbee3d51ff80654e97cd955169c075516d1dc
HtetThinzar15 <htetthinzarmon@seattleconsulting.co.jp>
2023-03-17 21:21:30 +0900

    cicd edit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "948366439019.dkr.ecr.ap-northeast-1.amazonaws.com/8g-cherry-team-rails-cicd"
+  config.hosts << "8gcherryteamrailscicd-env.eba-jhf3czg8.ap-northeast-1.elasticbeanstalk.com"
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
